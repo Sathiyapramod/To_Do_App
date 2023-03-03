1615f6a80399532eb41ad5e07410c5464623489d
Sathiyapramod <sathiyapramod22@gmail.com>
2023-03-03 23:36:54 +0530

    revised commits

--- src/Components/General.js
+++ src/Components/General.js
@@ -18,8 +18,7 @@ export const externalLinks ={
     color:'white'
 }
 
-export const Imageurl = "https://images.unsplash.com/photo-1606327054629-64c8b0fd6e4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
-
+export const Imageurl = "";
 export const themeStyling = {
    bgm: {
     backgroundImage: `url(${Imageurl})`,
